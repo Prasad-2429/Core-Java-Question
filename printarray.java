75a3f6cb443447a3d284b8b8f5e7da2ae23a9f74
Prasad Jadhav <prasadjadhavpatil9@gmail.com>
2025-08-10 02:21:47 +0530

    Java

---
+++ printarray.java
@@ -0,0 +1,12 @@
+public class printarray {
+    public static void main (String args[]){
+        int a [] = new int [3];
+        a[0] = 10;
+        a[1] = 20;
+        a[2] = 30;
+        for (int b : a)
+        {
+            System.out.print(b+" ");
+        }
+    }
+}
