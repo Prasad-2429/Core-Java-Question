696b271e10175ea4545dba77dfaa9c0c9805847d
Prasad Jadhav <prasadjadhavpatil9@gmail.com>
2025-08-11 16:26:20 +0530

    Looping Statement

---
+++ Whileloop.java
@@ -0,0 +1,18 @@
+public class Whileloop {
+    public static void main(String args[]){
+        //For multiple times output : 
+        /*int n ;
+        System.out.print("Enter value for condition \n");
+        Scanner ref =new Scanner (System.in);
+        n=ref.nextInt();
+        while(>=0){
+            System.out.print("Learn Coding");
+        }
+            */
+            int n=1;
+            while(n<=10){
+                System.out.println("Aryan");
+                ++n;
+            }
+    }
+}
